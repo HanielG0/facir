912b448d1c6a9dc9efcc89d17d6fd0f8977952dd
HanielG0 <54963302+HanielG0@users.noreply.github.com>
2019-10-23 20:18:08 -0300

    Create l6q7.c

---
+++ lista6resolvida/l6q7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main (){
+int n,m,x,o,a,l,i;
+    printf ("digite a primeira palavra: ");
+char n1[30];
+gets (n1);
+printf ("digite a segunda palavra: ");
+char n2[30];
+gets (n2);
+
+for(x=0;n1[x]!='\0';x++){
+        n=("%d",n1[x]);
+        m+=n;
+}
+for(x=0;n2[x]!='\0';x++){
+        o=("%d",n2[x]);
+        l+=o;
+}
+for(x=0;n2[x]!='\0';x++){
+        a=("%d",n2[x]);
+        i==a;
+}
+if (m>l){
+printf ("a primeira palavra e maior");
+}else{
+if (m<l){
+printf ("a segunda palavra e maior");
+}else{
+
+}
+}
+}
