7d6ca3aae97635a2617e46418585b30bdeacd205
HanielG0 <54963302+HanielG0@users.noreply.github.com>
2019-10-28 18:39:35 -0300

    Create projeto na fome.c

---
+++ lista6resolvida/projeto na fome.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct estoque{ 
+    int id;// identificador
+    int idf;// identificador de filmes
+char de[30];// data de entrada
+
+};
+typedef struct estoque Testoque;
+
+//inicio de função de estoque
+void cadastrarestoque(){
+int nfilme_estoque=0;
+int encerrar=0;
+
+Testoque estoque
+printf("\n   Pagina de Cadastro: \n\n");
+  printf("Digite o idf: ");
+   fflush(stdout);
+    gets(estoque.titulo);
